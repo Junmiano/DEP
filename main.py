6feec9aeede192824eb87df32c8219bcf5dafdc9
Junmiano <48517058+Junmiano@users.noreply.github.com>
2024-11-08 14:22:14 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,23 +7,23 @@ from pandas import DataFrame
 @st.cache_data
 def load_data(filepath: str) -> DataFrame:
     """
-    Load data from a CSV file located at the specified filepath.
+    Load data from a Excel file located at the specified filepath.
 
     Args:
-        filepath (str): The path to the CSV file to be loaded.
+        filepath (str): The path to the Excel file to be loaded.
 
     Returns:
         DataFrame: A DataFrame containing the loaded data, or an empty DataFrame if an error occurs.
 
     Raises:
-        FileNotFoundError: If the CSV file cannot be found at the specified path.
-        pd.errors.EmptyDataError: If the CSV file is empty.
+        FileNotFoundError: If the Excel file cannot be found at the specified path.
+        pd.errors.EmptyDataError: If the Excel file is empty.
         Exception: For any other exceptions that may occur during file loading.
     """
     try:
-        data = pd.read_csv(filepath)
+        data = pd.read_excel(filepath)
         if data.empty:
-            st.error("No data found in the CSV file.", icon="🚨")
+            st.error("No data found in the Excel file.", icon="🚨")
         return data
     except FileNotFoundError:
         st.error(
@@ -31,7 +31,7 @@ def load_data(filepath: str) -> DataFrame:
             icon="🚨",
         )
     except pd.errors.EmptyDataError:
-        st.error("No data found in the CSV file.", icon="🚨")
+        st.error("No data found in the Excel file.", icon="🚨")
     except Exception as e:
         st.error(f"An error occurred while loading the data: {e}", icon="🚨")
     return pd.DataFrame()  # Return an empty DataFrame if any error occurs
